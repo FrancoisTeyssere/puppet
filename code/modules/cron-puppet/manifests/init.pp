65f9eacd52dde29ba345eda705de93b0d6ed0c19
Francois Teyssere <francois.teyssere@gmail.com>
2017-04-07 11:03:34 +0200

    First commit of code directory

--- code/modules/cron-puppet/manifests/init.pp
+++ code/modules/cron-puppet/manifests/init.pp
@@ -1,7 +1,7 @@
 class cron-puppet {
     file { 'post-hook':
         ensure  => file,
-        path    => '/etc/puppetlabs/puppet/.git/hooks/post-merge',
+        path    => '/etc/puppetlabs/.git/hooks/post-merge',
         source  => 'puppet:///modules/cron-puppet/post-merge',
         mode    => 0755,
         owner   => root,
@@ -9,7 +9,7 @@ class cron-puppet {
     }
     cron { 'puppet-apply':
         ensure  => present,
-        command => "cd /etc/puppetlabs/puppet ; /usr/bin/git pull",
+        command => "cd /etc/puppetlabs ; /usr/bin/git pull",
         user    => root,
         minute  => '*/30',
         require => File['post-hook'],
