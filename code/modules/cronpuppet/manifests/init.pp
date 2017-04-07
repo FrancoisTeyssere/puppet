0be15faeb15af4442de64876a9164849aea4ee17
Francois Teyssere <francois.teyssere@gmail.com>
2017-04-07 12:41:47 +0200

    Changed the name cron-puppet to cronpuppet, it seems the parser could not handle the slash dot

---
+++ code/modules/cronpuppet/manifests/init.pp
@@ -0,0 +1,18 @@
+class cronpuppet {
+    file { 'post-hook':
+        ensure  => file,
+        path    => '/etc/puppetlabs/.git/hooks/post-merge',
+        source  => 'puppet:///modules/cronpuppet/post-merge',
+        mode    => '0755',
+        owner   => root,
+        group   => root,
+    }
+    cron { 'puppet-apply':
+        ensure  => present,
+        command => "cd /etc/puppetlabs ; /usr/bin/git pull",
+        user    => root,
+        minute  => '*/30',
+        require => File['post-hook'],
+    }
+}
+
